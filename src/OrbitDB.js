9aa3f7ab6c38ce9fa0728431dd06441b2125fa3b
shamb0t <shams.methnani@gmail.com>
2019-05-16 17:37:48 +0100

    support windows paths

--- src/OrbitDB.js
+++ src/OrbitDB.js
@@ -370,7 +370,7 @@ let databaseTypes = {
   // Save the database locally
   async _addManifestToCache (directory, dbAddress) {
     const cache = await this._loadCache(directory, dbAddress)
-    await cache.set(path.join(dbAddress.toString(), '_manifest'), dbAddress.root)
+    await cache.set([dbAddress.toString(), '_manifest'].join('/'), dbAddress.root)
     logger.debug(`Saved manifest to IPFS as '${dbAddress.root}'`)
   }
 
@@ -396,7 +396,7 @@ let databaseTypes = {
     if (!cache) {
       return false
     }
-    const data = await cache.get(path.join(dbAddress.toString(), '_manifest'))
+    const data = await cache.get([dbAddress.toString(), '_manifest'].join('/'))
     return data !== undefined && data !== null
   }
 
